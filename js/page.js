a9efbba1d8dbd90a28628ea54dbd65d2ba33a61c
minottj <minottj@clarkson.edu>
2023-11-09 12:16:11 -0500

    started

---
+++ js/page.js
@@ -0,0 +1,34 @@
+
+function runGame(x){
+    let x = playerChoice;
+
+    let randNumb = Math.floor(Math.random() * 2 + 1);
+
+    let flipResult; 
+
+
+
+    if (randNumb === 1){
+        flipResult = 'tails';
+    } else {
+        flipResult = 'heads';
+    }
+
+    let gameResult;
+
+    if (playerChoice === flipResult){
+        gameResult = 'WIN'
+    } else {
+        flipResult = 'LOSS'
+    }
+
+
+    console.log(flipResult);
+}
+
+let predicition = document.getElementById('prediction');
+
+let flip = document.getElementById('flip');
+
+let result = document.getElementById('result');
+result.innerText = gameREsult;
