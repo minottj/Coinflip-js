20775802e22fd1e94a0d302f13c2bd278e2250cf
minottj <minottj@clarkson.edu>
2023-11-28 12:07:37 -0500

    started to work

--- js/page.js
+++ js/page.js
@@ -1,34 +1,43 @@
-
-function runGame(x){
-    let x = playerChoice;
-
-    let randNumb = Math.floor(Math.random() * 2 + 1);
-
-    let flipResult; 
+function runGame(x) {
+    let playerChoice = x;
+    let randNumber = Math.floor(Math.random() * 2 + 1);
 
 
 
-    if (randNumb === 1){
-        flipResult = 'tails';
-    } else {
-        flipResult = 'heads';
-    }
-
-    let gameResult;
-
-    if (playerChoice === flipResult){
-        gameResult = 'WIN'
-    } else {
-        flipResult = 'LOSS'
-    }
-
-
-    console.log(flipResult);
+    console.log(randNumber);
 }
 
-let predicition = document.getElementById('prediction');
 
-let flip = document.getElementById('flip');
+// function runGame(x) {
+//     let x = playerChoice;
 
-let result = document.getElementById('result');
-result.innerText = gameREsult;
+//     let randNumb = Math.floor(Math.random() * 2 + 1);
+
+//     let flipResult; 
+
+
+
+//     if (randNumb === 1){
+//         flipResult = 'tails';
+//     } else {
+//         flipResult = 'heads';
+//     }
+
+//     let gameResult;
+
+//     if (playerChoice === flipResult){
+//         gameResult = 'WIN'
+//     } else {
+//         flipResult = 'LOSS'
+//     }
+
+
+//     console.log(flipResult);
+// }
+
+// let predicition = document.getElementById('prediction');
+
+// let flip = document.getElementById('flip');
+
+// let result = document.getElementById('result');
+// result.innerText = gameREsult;
